8929fdffbcedd9da0d7c672ab34ca2019ad5be36
Rexarrior <Rexarrior@yandex.ru>
2019-09-29 11:15:27 +0300

    df

--- fil_db_by_measure.py
+++ fil_db_by_measure.py
@@ -1,4 +1,5 @@
 from core.models import *
+from rtfm_server.settings import *
 import random
 from datetime import datetime
 x1 = 50
@@ -18,5 +19,5 @@ for i in range(x1, x2):
             y = j + random.random()
             m = Measure(user_id=user, operator_id=operator, network_id=network, latitude=x, longitude=y,time=datetime.now(), signal=(x+y)/(x2+y2))
             m.save()
-            
+
     
